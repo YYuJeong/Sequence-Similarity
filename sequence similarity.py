81bce4f6a7584f6fbce1d5ac801f40cc587e30fb
YangYuJeong <diddbwjd96@sookmyung.ac.kr>
2019-05-27 17:39:58 +0900

    no message

--- sequence similarity.py
+++ sequence similarity.py
@@ -13,8 +13,9 @@ from math import *
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
-str1 = "cehup"
+str1 = "cchup"
 str2 = "iamtf"
 str1Len = len(str1)
 str2Len = len(str2)
@@ -89,8 +90,6 @@ def editDistDP(str1, str2):  #Dynamic Programming
     return matrix[str1Len][str2Len], matrix 
 
 def NewLevenshteinDistance(str1, str2):
-    str1Len = len(str1)
-    str2Len = len(str2)
     matrix = [[0 for x in range(str2Len + 1)] for x in range(str1Len + 1)]   
     for i in range(str1Len + 1): 
         for j in range(str2Len + 1): 
@@ -109,7 +108,9 @@ def NewLevenshteinDistance(str1, str2):
     return matrix[str1Len][str2Len], matrix 
 
 def ComputeDiagonalCost(matrix, i, j, str1, str2, root):
+    print("tree start")
     maxlength = SearchLongestPath(root)
+    '''Levenshtein
     if ((matrix[i-1][j] + 1) > matrix[i-1][j-1]) and ((matrix[i][j-1] + 1) > matrix[i-1][j-1]):
         str1char = findall(root, filter_=lambda node: node.name in (str1[i-1]))
         str2char = findall(root, filter_=lambda node: node.name in (str2[j-1]))
@@ -125,6 +126,24 @@ def ComputeDiagonalCost(matrix, i, j, str1, str2, root):
         cost = round(itempath/maxlength, 3)
     else:
         cost = 1
+    '''
+    print("str1: ", str1)
+    print("str2: ", str2)
+    print("matrix: ", matrix)
+    str1char = findall(root, filter_=lambda node: node.name in (str1[i-1]))
+    str2char = findall(root, filter_=lambda node: node.name in (str2[j-1]))
+    str1char = str(str1char)
+    str2char = str(str2char)
+    str1lst = str1char.split('/')
+    str2lst = str2char.split('/')
+    for l in range(min(len(str1lst), len(str2lst))):
+        if str1lst[l] != str2lst[l]:
+            cmpindex = l
+            break
+    itempath = (len(str1lst)-cmpindex)+(len(str2lst)-cmpindex)
+    cost = round(itempath/maxlength, 3)
+
+
     return cost
 
 def SearchLongestPath(root):
@@ -157,9 +176,9 @@ def ComputeLevenshteinSimilarity(LevenshteinDist, str1, str2):
 def PrintMatrix(matrix, str1, str2):
     str1Len = len(str1)
     str2Len = len(str2)
-    print('{:4s}'.format('    -   '), end=" ")
+    print('{:5s}'.format('    -   '), end="  ")
     for i in range(str2Len):
-        print('{:4s}'.format(str2[i]), end=" ")
+        print('{:5s}'.format(str2[i]), end=" ")
     print(" ")
     for i in range(str1Len + 1):
         if i > 0:
@@ -168,7 +187,7 @@ def PrintMatrix(matrix, str1, str2):
             print("-", end=" ")
         print("[", end=" ")
         for j in range(str2Len + 1):
-          print('{:4s}'.format(str(matrix[i][j])), end=" ")
+          print('{:5s}'.format(str(matrix[i][j])), end=" ")
         print("]")
     print("")
     
@@ -177,6 +196,101 @@ def generateRandomSequence(size, chars=string.ascii_lowercase):
 
 
 
+#######################  Needleman-Wunsch #################################
+gap_penalty = -1
+match_award = 1
+mismatch_penalty = -2
+
+seq1 = "aabbcc"
+seq2 = "abc"
+
+def zeros(rows, cols):
+    retval = []
+    for x in range(rows):
+        retval.append([])
+        for y in range(cols):
+            retval[-1].append(0)
+    return retval
+
+def match_score(alpha, beta):
+    if alpha == beta:
+        print("match")
+        return match_award
+    else:
+        print("mismatch")
+        return mismatch_penalty
+
+def needleman_wunsch(seq1, seq2):
+    n = len(seq1)
+    m = len(seq2)  
+    
+    score = zeros(m+1, n+1)
+    for i in range(0, m + 1):
+        score[i][0] = gap_penalty * i
+    
+    for j in range(0, n + 1):
+        score[0][j] = gap_penalty * j
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            match = score[i - 1][j - 1] + match_score(seq1[j-1], seq2[i-1])
+            #cost = ComputeDiagonalCost(score, i, j, seq1, seq2, root)
+            #match = score[i - 1][j - 1] + cost
+            delete = score[i - 1][j] + gap_penalty
+            insert = score[i][j - 1] + gap_penalty
+            score[i][j] = max(match, delete, insert)
+
+    PrintMatrix(score, seq2, seq1)
+    print("NW score: ", score[m][n])
+    return score
+'''
+def needleman_wunsch_align(seq1, seq2): 
+    n = len(seq1)
+    m = len(seq2) 
+    score = needleman_wunsch(seq1, seq2)
+    PrintMatrix(score, seq2, seq1 )
+    align1 = ""
+    align2 = ""
+
+    i = m
+    j = n
+
+    while i > 0 and j > 0:
+        score_current = score[i][j]
+        score_diagonal = score[i-1][j-1]
+        score_up = score[i][j-1]
+        score_left = score[i-1][j]
+
+        #if score_current == score_diagonal + match_score(seq1[j-1], seq2[i-1]):
+        cost = ComputeDiagonalCost(score, i, j, seq1, seq2, root)
+        if score_current == score_diagonal + cost:
+            align1 += seq1[j-1]
+            align2 += seq2[i-1]
+            i -= 1
+            j -= 1
+        elif score_current == score_up + gap_penalty:
+            align1 += seq1[j-1]
+            align2 += '-'
+            j -= 1
+        elif score_current == score_left + gap_penalty:
+            align1 += '-'
+            align2 += seq2[i-1]
+            i -= 1
+
+    while j > 0:
+        align1 += seq1[j-1]
+        align2 += '-'
+        j -= 1
+    while i > 0:
+        align1 += '-'
+        align2 += seq2[i-1]
+        i -= 1
+    
+    align1 = align1[::-1]
+    align2 = align2[::-1]
+    
+    return(align1, align2)
+'''    
 #######################  Dynamic Time Warping #################################
 def DTW(A, B, window=sys.maxsize, d=lambda x, y: abs(x - y)):
     A, B = np.array(A), np.array(B)
@@ -199,10 +313,30 @@ def DTW(A, B, window=sys.maxsize, d=lambda x, y: abs(x - y)):
     path.append((0, 0))
     return cost[-1, -1], path
 
-
+def StringToArrary(str1, str2):
+    stringAll = str1 + str2
+    stringAll_list = list(stringAll)
+    stringAll_list = list(set(stringAll_list))
+    stringAll_list.sort()
+    stringDic = defaultdict(lambda : None)
+    for i in range(len(stringAll_list)):
+        stringDic[stringAll_list[i]] = (i+1)
+    str1Num, str2Num = [], []
+    for i in range(len(str1)):
+        str1Num.append(stringDic[str1[i]])
+    print(str1Num)
+    for i in range(len(str2)):
+        str2Num.append(stringDic[str2[i]])
+        
+    return
+    
+        
 def DTW_main():
-    A = np.array([1, 1, 2, 2, 2, 3, 3, 3])
-    B = np.array([1, 2, 3])
+    str1 = "ABC"
+    str2 = "BCD"
+
+    
+    M, N = len(str1Num), len(str2Num)
     cost, path = DTW(A, B, window = 6)
     print('Total Distance is ', cost)
     offset = 6
@@ -212,99 +346,6 @@ def DTW_main():
     for (x1, x2) in path:
         plt.plot([x1, x2], [A[x1], B[x2] + offset])
     plt.show()
-
-
-#######################  Needleman-Wunsch #################################
-
-def print_matrix(mat):
-    for i in range(0, len(mat)):
-        print("[", end = "")
-        for j in range(0, len(mat[i])):
-            print(mat[i][j], end = "")
-            if j != len(mat[i]) - 1:
-                print("\t", end = "")
-        print("]\n")
-
-gap_penalty = -1
-match_award = 1
-mismatch_penalty = -1
-
-seq1 = "AAABBBCCC"
-seq2 = "A"
-
-def zeros(rows, cols):
-    retval = []
-    for x in range(rows):
-        retval.append([])
-        for y in range(cols):
-            retval[-1].append(0)
-    return retval
-
-def match_score(alpha, beta):
-    if alpha == beta:
-        return match_award
-    elif alpha == '-' or beta == '-':
-        return gap_penalty
-    else:
-        return mismatch_penalty
-
-def needleman_wunsch(seq1, seq2):
-    n = len(seq1)
-    m = len(seq2)  
-    
-    score = zeros(m+1, n+1)
-    for i in range(0, m + 1):
-        score[i][0] = gap_penalty * i
-    
-    for j in range(0, n + 1):
-        score[0][j] = gap_penalty * j
-    
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            match = score[i - 1][j - 1] + match_score(seq1[j-1], seq2[i-1])
-            delete = score[i - 1][j] + gap_penalty
-            insert = score[i][j - 1] + gap_penalty
-            score[i][j] = max(match, delete, insert)
-    print(np.shape(score))
-    print("NW score: ", score[m][n])
-    return score
-
-def needleman_wunsch_align(score, seq1, seq2):
-    n = len(seq1)
-    m = len(seq2)  
-    align1 = ""
-    align2 = ""
-    i = m
-    j = n
-    score_current = score[i][j]
-    score_diagonal = score[i-1][j-1]
-    score_up = score[i][j-1]
-    score_left = score[i-1][j]
-    if score_current == score_diagonal + match_score(seq1[j-1], seq2[i-1]):
-        align1 += seq1[j-1]
-        align2 += seq2[i-1]
-        i -= 1
-        j -= 1
-    elif score_current == score_up + gap_penalty:
-        align1 += seq1[j-1]
-        align2 += '-'
-        j -= 1
-    elif score_current == score_left + gap_penalty:
-        align1 += '-'
-        align2 += seq2[i-1]
-        i -= 1
-    while j > 0:
-        align1 += seq1[j-1]
-        align2 += '-'
-        j -= 1
-    while i > 0:
-        align1 += '-'
-        align2 += seq2[i-1]
-        i -= 1
-    align1 = align1[::-1]
-    align2 = align2[::-1]
-
-    return(align1, align2)
 
 
 if __name__ == '__main__':
@@ -339,12 +380,9 @@ if __name__ == '__main__':
 
     print("=="*30)   
     print("< Needleman-Wunsch Measure >")
-    print_matrix(needleman_wunsch(seq1, seq2))
-    score1 =  needleman_wunsch(seq1, seq2) 
-    
-    output1, output2 = needleman_wunsch_align(score1 ,seq1, seq2)
-    
-    print(output1 + "\n" + output2)
+    needleman_wunsch(seq1, seq2)
+    #output1, output2 = needleman_wunsch_align(seq1, seq2)
+    #print("output1: "+ output1 + "\noutput2: " + output2)
     
 
 
