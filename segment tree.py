35e4867e0dd6b1d8f496275179f4f21294865956
YYuJeong <a92983469@gmail.com>
2019-09-27 17:18:06 +0900

    - 코드 구현 완료

--- segment tree.py
+++ segment tree.py
@@ -52,15 +52,15 @@ global no2serial
 global serial2no
 global locInTrip
 global depth
-global n
+global k
 global rangeMin
 #n = 13
-n = 13
-no2serial = [-1 for i in range(n)]
-serial2no = [-1 for i in range(n)]
+k = 13
+no2serial = [-1 for i in range(k)]
+serial2no = [-1 for i in range(k)]
 
-locInTrip = [-1 for i in range(n)]
-depth = [-1 for i in range(n)]
+locInTrip = [-1 for i in range(k)]
+depth = [-1 for i in range(k)]
 
 nextSerial = 0
 def traverse(here, d, trip):
@@ -106,14 +106,16 @@ global count
 count = 0
 def RMQ(array):
     global rangeMin
+    global n
     n = len(array)
-    print("n:", n)  
+ #   print("n:", n)  
     rangeMin = []
     rangeMin = [0 for i in range(n*4)]   
-    print("rangeMin size: ", len(rangeMin))
+  #  print("rangeMin size: ", len(rangeMin))
     init(array, 1, 0 , n-1)
 
 def init(array, node, left, right):
+    '''
     global count
     count = count + 1
     print("count : " , count )
@@ -133,17 +135,17 @@ def init(array, node, left, right):
     print("init left: ", left)
     print("init right: ", right)
     print("*"*50)
-     
+    ''' 
     if left == right: 
-        print("좌 우 같음")
+       # print("좌 우 같음")
         rangeMin[node] = array[left]
-        print("if rangeMin[node]: ", rangeMin[node])
+       # print("if rangeMin[node]: ", rangeMin[node])
         return rangeMin[node]
     mid = int((left + right)/2)
-    print("mid: ", mid)
+  #  print("mid: ", mid)
     rangeMin[node] = min(init(array, node * 2 + 1, mid + 1, right), init(array, node * 2, left, mid))
     #rangeMin[node] = min(init(array, 1 * 2, 0, 12), init(array, 1 * 2 + 1, 12 + 1, 24))
-    print("min rangeMin[node]: " , rangeMin[node])
+   # print("min rangeMin[node]: " , rangeMin[node])
     return rangeMin[node]
 
 
@@ -154,22 +156,49 @@ def prepareRMQ():
     traverse(0, 0 , trip)
     return RMQ(trip)
 
-'''
+
 def distance(u, v):
+    #global locInTrip
+    for i in range(0, len(locInTrip)):
+        print("locInTrip[", i , "]: " , locInTrip[i])
     lu = locInTrip[u]
     lv = locInTrip[v]
+    print("lu: "  , lu)
+    print("lv: " , lv)
     if(lu > lv):
         lu, lv = lv, lu
+    print("쿼리: " , query1(lu, lv))
     lca = serial2no[query1(lu, lv)]
     return depth[u]+depth[v]-2*depth[lca]
 
 
 def query1(left, right):
-    return query (left, right , 1, 0 , n-1)
+    global n
+    print("arg2 query")
+    print("n: " , n)
+    return query(left, right , 1, 0 , n-1)
 
 def query(left, right, node, nodeLeft, nodeRight):
+    global count
+    print("*"*50)
+    count = count + 1
+    print("count : " , count )
+    print("init node: ", node)
+    print("init left: ", left)
+    print("init right: ", right)
+    print("nodeLeft : ", nodeLeft)
+    print("nodeRight: " , nodeRight)
+    if (right < nodeLeft) or (nodeRight < left):
+        print("max int : ")
+        return sys.maxsize
+    if (left <= nodeLeft) and (nodeRight <= right):
+        print("노드가 완전 포함: " , rangeMin[node])
+        return rangeMin[node]
+    mid = int((nodeLeft + nodeRight)/2)
+    print("mid: " , mid)
+    return min(query(left, right, node * 2 + 1, (mid+1), nodeRight), query(left, right, node * 2, nodeLeft, mid))
     
-'''   
+  
      
 if __name__ == '__main__':
     
@@ -199,7 +228,7 @@ if __name__ == '__main__':
     
     prepareRMQ()
     
-    distance(2, 7)
+    print(distance(10, 0))
 
     
     
